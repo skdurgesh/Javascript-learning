a02497ece4c2f76728bd1c75d7e0cb016e9e4583
Durgesh Singh <skdurgesh@users.noreply.github.com>
2025-04-29 19:50:38 +0000

    new files: convoperation, comparison and datatypes summary added.

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -15,5 +15,35 @@ let booleanValue = Boolean(isloggedIn);
 
 let someNumber = 30;
 let stringNum = String(someNumber);
-console.log(stringNum);
-console.log(typeof stringNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(3 + 4);
+// console.log(3 * 4);
+// console.log(3 - 4);
+// console.log(3 / 4);
+// console.log(3 % 4);
+// console.log(3 ** 4);
+
+let str1 = "Hello";
+let str2 = " Durgesh";
+// console.log(str1 + str2); // Concatenation
+
+// console.log(1 + "1");
+// console.log("1" + 1);
+// console.log(2 + 2 + "1");
+// console.log("1" + 2 + 2);
+// console.log((3 + 4) * 5 % 3);
+//console.log(true);
+
+let x = 4;
+const y = x++;
+console.log(`x: ${x}, y: ${y}`);
+
+let a = 4;
+const b = ++a;
+console.log(`a: ${a}, b: ${b}`);
---
+++ 01_Basics/Datatypes-summary.js
@@ -0,0 +1,48 @@
+// Primtive
+
+// 7 types of primitive data types
+// These data types are used call by value there value get copied from there original doesn't change.
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. BigInt
+
+const score = 101;  // number
+const scoreDec = 100.3; // number
+
+const isLoggedIn = false; // boolean
+const outsideTemp = null; // Object
+let userEmail; // undefined
+
+const id = Symbol('123'); //symbol
+const secondId = Symbol('123'); // symbol
+
+//console.log(id === secondId);
+const BigNumber = 1234567890123456789012345678901234567890n;
+
+
+// reference type (non primitive)
+// Array, objects, functions
+
+const heroes = ['Shaktiman', 'Dhruv', 'Doga', 'Naagraaj', 'Bankelal', 'Chacha Chaudhary']; // Object
+let myObj = {
+    name: "Durgesh",
+    occupation: 'Software Engineer',
+    age: 39,
+    ismarried: true,
+    hobbies: ['Cricket', 'Reading', 'Travelling'],
+    address: {
+        city: 'Delhi',
+        state: 'Delhi',
+        country: 'India'
+    },
+} // Object
+
+const myFunction = function() {
+    console.log('Hello World');
+} // Function Object
+
+console.log(typeof myFunction);
